bde06937388e42aa30fdf61cfecfd8fe80ee0144
Scott <hscottvo@gmail.com>
2026-07-21 23:55:04 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Rust dev shell";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs [ "aarch64-darwin" "x86_64-linux" ] (
+          system: f nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            bacon
+            cargo
+            clippy
+            rustc
+            rustfmt
+            rust-analyzer
+          ];
+        };
+      });
+    };
+}
